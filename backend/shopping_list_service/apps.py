169d7108394255a8484e07a1437c31a802af9062
Александр Ермаков <uraltheoretic@yandex.ru>
2023-09-23 20:13:46 +0300

    Новые правки

--- backend/shopping_list_service/apps.py
+++ backend/shopping_list_service/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ShoppingListServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shopping_list_service'
+    verbose_name = 'Сервис покупок'
