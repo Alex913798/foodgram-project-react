b2e3f5e52a168a443a66f59b35df7dcde575b35c
Александр Ермаков <uraltheoretic@yandex.ru>
2023-09-27 01:05:42 +0300

    Edit

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -7,5 +7,6 @@ class AuthorOrReadOnly(permissions.BasePermission):
                 or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user)
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
