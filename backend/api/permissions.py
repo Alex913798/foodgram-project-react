d4692164302db02e7257a4dca83bb2c03dfa8b68
Александр Ермаков <uraltheoretic@yandex.ru>
2023-09-28 04:02:32 +0300

    edit

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -7,8 +7,7 @@ class AuthorAdmin(permissions.BasePermission):
                 or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_superuser
+        return (request.user.is_superuser
                 or request.user.is_staff
                 or obj.author == request.user)
 
